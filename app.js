2a0f5cc83a0db92bcf730903dcacfacee599559c
anjaedman <anjaedman@hotmail.com>
2022-10-27 10:21:59 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+
+
+// 1.
+// let firstName;
+// 2.
+// firstName = anja;
+3. 
+// let x = "Greta;"
+// console.log (x)
+
+// = Greta
+
+// 4.
+// let flower;
+// let flower = "rose";
+
+// let tree;
+// let tree = "ash";
+
+// 5.
+
+// if (i == 5);
+// svar: a
+
+// 6.
+
+// if (i != 5);
+
+// 7.
+
+// while (i <=10);
+
+// 8.
+// for (i = 0; i <= 5; i++);
+
+// 9.
+// var colors = ["red", "green", "blue"];
+
+// 10.
+
+// let carName;
+
+// 7.
+// =
+
+// 8.
+
+// true
+
+// 9.
+// ja
+
+// 10.
+// let enArray = [];
+
+// 10.
+// const enArray = [];
+
+// 11.
+// function myFunction()
+
+// 12.
+// myFunction();
+
+// 13.
+// Array
+
+// 14.
+// Toe, sista av ändringar
+
+// 15.
+// Hardy
+
+// 16.
+// clara
+
+// 17.
+
+// false
+
+// 18.
+// Ja
+
+// 19.
+
+// false?
+
+
+20.
+
+
+
+
+
