b479ae5234203004d05a8159f73ff0d1f30053ed
anjaedman <anjaedman@hotmail.com>
2022-11-02 11:50:21 +0100

    uppdatering övningsuppgifter

--- app.js
+++ app.js
@@ -86,9 +86,527 @@
 // false?
 
 
-20.
+// 20.
+
+// 1a: undefined
+// 2a: 10
+
+// 21.
+
+// let myvar = "abc" + true + 10;
+// console.groupCollapsed(myvar);
+
+// abctrue10
+
+// 22. ???
+
+// function checkValue(x) {
+// if (isNaN(x)) {
+// return NaN;
+// }
+// return x;
+// }
+
+// console.log(checkValue ("5"));
+// console.log(checkValue ("Any value"));
+
+// 5
+// NaN
+// 5 och NaN
+
+// 23.
+// var a=0;
+// do{
+// a++;
+// console.log(a);
+// } while(false);
+
+// 1 gång
+
+// 24.
+
+// const students = ["John", "Ann", "Aaron", "Edwin",
+// "Elizabeth"];
+// for (i=0; i<students.length; i++)
+// {
+// console.log(students[i]);
+// }
+
+// svar:
+// John
+// Ann
+// Aaron
+// Edwin
+// Elizabeth
+
+
+// 25.
+
+// const students = ["John", "Ann", "Aaron", "Edwin",
+// "Elizabeth"];
+// for (i=0; i<students.length - 1; i++)
+// {
+// console.log(students[i]);
+// }
+
+// svar: 
+// John
+// Ann
+// Aaron
+// Edwin
+
+// 26. varför false?
+
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 == 0.3);
+
+// 0.30000000000000004
+// false
+
+// 27.
+// console.log(1 + "2" + "2");
+// 122
+
+// 28. ?
+
+// console.log(1 + +"2" + "2");
+// 32
+
+// 29.
+
+// console.log(1 + -"1" + "2");
+// 02
+
+// 30.
+// console.log(+"1" + "1" + "2");
+// 112
+
+// 31.
+
+// console.log( "A" - "B" + "2");
+
+// NaN2
+
+// 32.
+
+// console.log( "A" - "B" + 2);
+
+// NaN
+
+// 33. ?????
+
+// let bar = true;
+// console.log(bar + 0);
+// console.log(bar + "xyz");
+// console.log(bar + true);
+// console.log(bar + false);
+
+// 1
+// truexyz
+// 2
+// 1
+
+// 34. ?
+
+// function hello(tjao) {
+//     return "Hello world!" + tjao;
+// }
+// console.log(hello(" "));
 
 
 
+// 35.
+
+// function hello() {
+// return 'Hej!';
+// }
+// let x = hello();
+
+// console.log (hello)
+
+// svar: hello
+
+// 36. förstår koden men .. let x = h1 + ' ' + h2; dubbelfnurparna?
+
+// function hej(name) {
+//     return 'Hej ' + name + '!';
+//     }
+//     let h1 = hej('Karin');
+//     let h2 = hej('Jonas');
+//     let x = h1 + ' ' + h2;
+
+// console.log(x)
+
+
+// 37. vet inte och får inte till det?
+
+// function log() {
+// console.log(”Hejsan Console!”);
+// };
+
+// function log(console.log('Hello Console!');) {
+// };
+
+// console.log(log)
+
+// 38.
+// Brendan Eich
+
+
+
+
+
+
+// odinprojektet
+
+// const max = 57;
+// let actual = max - 13;
+// let percentage = actual / max;
+
+// console.log(percentage)
+
+
+// function troubleshooting() {
+// 	const a = 1;
+// 	const b = 1;
+
+// 	let result;
+
+// 	result = a + b;
+
+// }
+// console.log(result);
+
+// function veta() {
+//     const a = 2;
+//     const b = 3;
+
+//     let ratt = a + b;
+
+
+//     console.log(ratt)
+// }
+
+// let thomasDenBäste;
+
+// thomasDenBäste = "Jag är bäst!!!";
+
+// let sant = true;
+// /*mitt svar: c*/
+// console.log(thomasDenBäste + sant);
+
+// let namet =  prompt("Skriv ditt namn.");
+
+// alert("Hejsan " + namet + " " + thomasDenBäste + sant);
+
+
+
+// let kundlista;
+
+// let kundLista = "firstname + lastname + gatuadress + postnummer + ort +  mobil + mail";
+
+// console.log(kundLista);
+
+// let sant = "\nVi skickar information, snarast! \nVälkommen!";
+
+// let firstname =  prompt("Skriv ditt Förnamn.");
+// let lastname =  prompt("Skriv ditt Efternamn.");
+// let gatuadress =  prompt("Skriv din Gatuadress.");
+// let postnummer =  prompt("Skriv ditt Postnummer.");
+// let ort =  prompt("Skriv din Bostadsort.");
+// let mobil =  prompt("Skriv ditt Mobilnummer.");
+// let mail =  prompt("Skriv din Mailadress.");
+
+// alert("Tack! " + firstname + " " + sant);
+
+
+// console.log(typeof ( 3 + "4" - 2 ))
+// console.log( 3 + "4" - 2 )
+
+// 1.1 Number, string, objekt, booleans
+
+1.2
+// a
+// console.log(typeof (1 + 1 ))
+// console.log( 1 + 1  )
+
+// number
+// 2
+
+// B
+// console.log(typeof (2.5 + 2.5 ))
+// console.log(2.5 + 2.5 )
+// number
+// 5
+
+// C
+// console.log(typeof (10 + '10' ))
+// console.log(10 + '10' )
+// string
+// 1010
+
+// D
+// console.log(typeof (10 - '10' ))
+// console.log(10 - '10' )
+
+// number
+// 0
+
+// E
+
+// console.log(typeof (3 + '4' - 2  ))
+// console.log(3 + '4' - 2 )
+// number
+// 32
+
+// F
+
+// console.log(typeof (Number('5') ))
+// console.log(Number('5')  )
+// number
+// 5
+
+// G
+// console.log(typeof (Number('swordfish')  ))
+// console.log(Number('swordfish') )
+// number
+// NaN
+
+
+// H
+
+// console.log(typeof ( 3 == '3'  ))
+// console.log( 3 == '3' )
+// boolean
+// true
+
+// I
+
+// console.log(typeof ( 3 === '3' ))
+// console.log( 3 === '3' )
+// boolean
+// false
+
+// J
+
+// console.log(typeof ( 'två' < 'tre' )
+// console.log('två' < 'tre' )
+// boolean
+// false
+
+// 1:3
+// a)
+
+// let x, y, z;
+// console.log(z = 5);
+// 5
+
+// b)
+
+// let x, y, z;
+// console.log (z++) ;
+// NaN
+
+// C)
+
+// let x, y, z;
+// console.log (--z) ;
+// NaN
+
+// D)
+
+// let x, y, z;
+// console.log (z += 15) ;
+// NaN
+
+// E)
+// let x, y, z;
+// x = 8; y = 16; z = y - x ;
+// console.log( z );
+// 8
+
+// F)
+// let x, y, z;
+// x = 10; z = x+=1;
+// console.log( z );
+// 10
+
+// G)
+
+// let x, y, z;
+// x = 2; y = 5; x = x + y; y = x + y; z = y; 
+// console.log( z );
+
+// z= 12
+// x= 7
+// y= 12
+
+// 1.4
+
+// let text = 'Gädda'; 
+// let result = text.toUpperCase();
+// console.log(result) 
+
+// 1.5
+
+// let user = 'Nisse', city = 'Göteborg' 
+// // Skriv ut: "Hello Nisse from Göteborg" 
+
+// console.log("Hello " + user  + " from " + city );
+
+// 2.1
+
+// for( let i=2; i<7; i++ ) { 
+//     if( i!= 5 ) 
+//     console.log('nummer ' + i); 
+// } 
+// nummer 2
+// nummer 3
+// nummer 4
+// nummer 6
+
+// 2.2
+
+
+// let text = "hej"
+// for(i=1; i<6; i+=1)
+// console.log(text);
+
+// 2.3
+
+// const array = ['äpple', 'päron', 'banan'] 
+// for( let index=0; index<array.length; index++ ) { 
+//     console.log( array[index] ) } 
+// äpple
+// päron
+// banan
+
+// const array = [ "ljus", "tavlor", "smycken", "kakfat",]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+//     console.log(array[index]);
+
+// 2.4
+
+// const array = [ 2.5, 2.8, 3.5, 5.5,]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(array [index])
+    
+// }
+
+// 2.5
+
+// for( let i=10; i<16; i++ ) {  
+//         console.log( i); 
+//     } 
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+
+// 2.6
+
+// for( let i=9; i<19; i++ ) 
+// {  
+// if (i % 2 == 1) continue;
+// console.log( i); 
+// } 
+
+
+// 2.7
+
+// const array = ['äpple', 'päron', 'banan',] 
+// for ( let index=array.length -1; index >= 0; index--) { 
+//     console.log(array[index]);
+// } 
+
+
+// äpple
+// päron
+// banan
+
+// const array = [ "ljus", "tavlor", "smycken", "kakfat",]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+
+//     console.log(array[index]);
+//      }
+
+//      2.8
+
+
+// const array =  [ 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10]
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(array [index]);
+// }
+
+// 3 objekt
+
+// 3.1
+
+// let object = { x: true } 
+// object.y = false 
+// console.log(x)
+// console.log(y)
+// console.log(z)
+
+
+// inget, mycket odefenierat
+
+// 3.2
+
+// let userName = 'kalle'
+// let userAge = 20
+// let userScore = 10
+// let totalUserCount = 100 
+// let animalType = 'ekorre'
+// let animalFood = 'nötter'
+
+
+// const användare = (userName + " " + userAge);
+
+// const points = (userScore + " " + totalUserCount);
+
+// const animal = (animalType + " " + animalFood);
+
+// console.log("username + userage",användare);
+// console.log("userscore + totalusercount", points);
+// console.log("animaltype + animalfood", animal);
+
+// 3.3
+
+
+// const user = { 
+//     names: 'Sam', 
+//     cartValues: [19, 120, 200] 
+    
+// } 
+
+// function totalNumbers(number) {
+//     var total = 0;
+//     for (var i = 0; i < number.length; i++) {
+//       total += number[i];
+//     }
+//     return total;
+//   }
+
+//   function smartTotal(newNumber) {
+//     return newNumber.reduce((total, number) => total + number, 0);
+
+//   }
+
+// console.log(totalNumbers(user.cartValues))
+// console.log(smartTotal(user.cartValues))
+
+// 339
+// 339
+// 
+// 3.4
 
 
