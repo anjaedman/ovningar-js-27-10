dda97382976bdbc507e3dfca16c05eca798f5286
anjaedman <anjaedman@hotmail.com>
2022-11-08 13:52:15 +0100

    uppdatering 8/11

--- ovningar2.js
+++ ovningar2.js
@@ -123,44 +123,134 @@
 
 
 //  4.6
-let arrayOfFruits = ["Äpple", "Päron", "Banan"];
-
-// let shortMap = arrayOfFruits;
-
-let shortMap = [];
-
-function shortMap2 () { 
-    for (let i = 0; i < arrayOfFruits.length; i++) {
-        shortMap[i] = arrayOfFruits[i].substring(0,3)
-    }
-}
-
-console.log("arrayoffruits", arrayOfFruits);
-// console.log("shortmap", shortMap);
-console.log("shortmap2", shortMap2);
 
 
-// let arrayOfFruits = ["River", "Lake", "Pond", "Sea"];
+// Thomas lösningar
+
+
+// const arrayOfFruits = ["Äpple", "Päron", "Banan"];
+
+// console.log(arrayOfFruits);
 
 // let shortMap = [];
 
 // for (let i = 0; i < arrayOfFruits.length; i++) {
 
 //     shortMap[i] = arrayOfFruits[i].substring(0,3);
+// }
 
+// console.log(shortMap);
+
+// //The smarter one-liner way
+
+// const shortMap2 = arrayOfFruits.map( str => str.substring(0, 2) );
+
+// console.log("neewArray2 med map()-funktionen: " );
+
+// console.log( shortMap2 )
+
+
+
+
+
+// anjas thomas lösning
+
+// let arrayOfFruits = ["Äpple", "Päron", "Banan"];
+
+// let shortMap = [];
+
+// for (let i = 0; i < arrayOfFruits.length; i++) {
+
+//     shortMap[i] = arrayOfFruits[i].substring(0,3);
 // }
 
 // console.log(shortMap);
 
 
-// 4.7
 
-// const arrayOfFruits = ['äpple', 'päron', 'banan', 'grapefrukt', 'vindruva', 'lime'];
+// joakims lösning
 
-// function shortFilter(arrayOfFruits) {
-
-
+// const fruits = ['äpple', 'päron', 'banan']
+// const modifiedArray = shortMap(fruits)
+// console.log("modified array:: ", modifiedArray)
+// function shortMap(arr){
+//   const tempArray = []
+//   for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     tempArray.push(`${element}`.substring(0, 3))
+//   }
+//   return tempArray
 // }
 
-// console.log("arrayoffruits", arrayOfFruits);
-// console.log("shortfilter" shortFilter);
+// anjas sista lösning
+
+// const arrayOfFruits = ['äpple', 'päron', 'banan'];
+// const modifiedArray = shortMap(arrayOfFruits);  
+
+// function shortMap(arr){
+//     const tempArray = []
+//     for (let index = 0; index < arr.length; index++) {
+//         const element = arr[index];
+//         tempArray.push(`${element}`.substring(0, 3)) 
+//     }
+//     return tempArray
+// }
+// console.log(modifiedArray);   
+
+
+
+
+// 4.7 Skriv en funktion med namnet "shortFilter". 
+// Den ska ta en array med strängar och returnera en ny array. 
+// Den nya arrayen ska innehålla alla strängar från den gamla som är kortare än 7 tecken. 
+// Exempel array: ['äpple', 'päron', 'banan', 'grapefrukt', 'vindruva' 'lime'] 
+// (Grapefrukt och vindruva är för långa och ska inte vara med i den nya arrayen.)
+
+
+const fruits = ['äpple', 'päron', 'banan', 'grapefrukt', 'vindruva', 'lime'];
+
+console.log("fruits::", fruits);
+
+const ihop = shortFilter(fruits);
+
+function shortFilter(fruits) {
+    const tempArray = []
+    for (let index = 0; index < fruits.length; index++) {
+        const element = fruits[index];
+        tempArray.push(`${element}` .match(>= 7)) 
+    }
+    return tempArray
+}
+
+console.log("ihop::", ihop);
+
+
+
+
+
+// const fruits = ['äpple', 'päron', 'banan', 'grapefrukt', 'vindruva', 'lime'];
+
+// const modifiedArray = shortFilter(fruits);  
+
+// function shortFilter () {
+//     const tempArray =[]
+//     for (let index = 0; index < fruits.length; index++) {
+//         const element = fruits[index];
+//         tempArray.push(`${element}`.filter( >= 7 ))
+    
+//     }   
+//      return tempArray
+// }
+
+// console.log(modifiedArray);   
+
+
+
+// function shortFilter(arr){
+//     const tempArray = []
+//     for (let index = 0; index < arr.length; index++) {
+//         const element = arr[index];
+//         tempArray.push(`${element}`.substring(0, 3)) 
+//     }
+//     return element >= 7;
+// }
---
+++ ovningar3.js
@@ -0,0 +1,55 @@
+const arrayOfPersons = [
+    // 0
+    {
+      firstName: "Sulejman",
+      role: "Student",
+    },
+    // 1
+    {
+      firstName: "Jonas",
+      role: "Student",
+    },
+    // 2
+    {
+      firstName: "Anja",
+      role: "Student",
+    },
+    // 3
+    {
+      firstName: "Patric",
+      role: "Student",
+    },
+    // 4
+    {
+      firstName: "Michela",
+      role: "Student",
+    },
+    // 5
+    {
+      firstName: "Simon",
+      role: "Student",
+    },
+    {
+      firstName: "Joacim",
+      role: "Student",
+    },
+    {
+      firstName: "Jennifer",
+      role: "Student",
+    },
+    {
+      firstName: "Mirsada",
+      role: "Student",
+    },
+    {
+      firstName: "Moneer",
+      role: "Student",
+    },
+    {
+      firstName: "Olof",
+      role: "Student",
+    },
+    {
+      firstName: "Erik",
+      role: "Student",
+    },
